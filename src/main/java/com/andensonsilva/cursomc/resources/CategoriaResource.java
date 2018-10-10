1902b33adcdbf4f77f2bd15d5326efd8883de87b
Andenson Silva <andenson17@gmail.com>
2018-10-10 13:29:02 -0300

    Teste primeira classe domínio

--- src/main/java/com/andensonsilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andensonsilva/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.andensonsilva.cursomc.resources;
 
+import com.andensonsilva.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
     @GetMapping()
-    public String listar() {
-        return "It works";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
