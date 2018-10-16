ea7a0f9138c07a92c357053eaf66ca36ce980d40
Andenson Silva <andenson17@gmail.com>
2018-10-15 22:35:53 -0300

    Associacao muitos para muitos

--- src/main/java/com/andensonsilva/cursomc/CursomcApplication.java
+++ src/main/java/com/andensonsilva/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.andensonsilva.cursomc;
 
 import com.andensonsilva.cursomc.domain.Categoria;
+import com.andensonsilva.cursomc.domain.Produto;
 import com.andensonsilva.cursomc.repositories.CategoriaRepository;
+import com.andensonsilva.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,20 +14,36 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository repo;
+    @Autowired
+    private CategoriaRepository repo;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+    @Autowired
+    private ProdutoRepository prodRepo;
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
 
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+    @Override
+    public void run(String... args) throws Exception {
 
-		this.repo.saveAll(Arrays.asList(cat1, cat2));
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
 
-	}
+        Produto prod1 = new Produto(null, "computador", 1239.00);
+        Produto prod2 = new Produto(null, "Tv", 800.00);
+        Produto prod3 = new Produto(null, "Geladeira", 3002.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+        cat2.getProdutos().addAll(Arrays.asList(prod3));
+
+        prod1.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        prod1.getCategorias().addAll(Arrays.asList(cat2, cat1));
+
+        prod3.getCategorias().addAll(Arrays.asList(cat1));
+
+        this.repo.saveAll(Arrays.asList(cat1, cat2));
+        this.prodRepo.saveAll(Arrays.asList(prod1, prod2, prod3));
+
+    }
 }
