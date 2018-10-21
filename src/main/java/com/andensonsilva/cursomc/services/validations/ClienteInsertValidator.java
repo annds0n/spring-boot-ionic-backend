30d40fde6a647fdd8e89a8209db20fb0b43a6c23
Andenson Silva <andenson17@gmail.com>
2018-10-21 14:43:22 -0300

    valida se exixte e-mail cadastrado

--- src/main/java/com/andensonsilva/cursomc/services/validations/ClienteInsertValidator.java
+++ src/main/java/com/andensonsilva/cursomc/services/validations/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.andensonsilva.cursomc.services.validations;
 
+import com.andensonsilva.cursomc.domain.Cliente;
 import com.andensonsilva.cursomc.domain.enums.TipoCliente;
 import com.andensonsilva.cursomc.dto.ClienteNewDTO;
+import com.andensonsilva.cursomc.repositories.ClienteRepository;
 import com.andensonsilva.cursomc.resources.exceptions.FieldMessage;
 import com.andensonsilva.cursomc.services.validations.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public boolean isValid(ClienteNewDTO clienteNewDTO, ConstraintValidatorContext constraintValidatorContext) {
@@ -23,6 +29,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if(clienteNewDTO.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = this.clienteRepository.findByEmail(clienteNewDTO.getEmail());
+
+        if(aux != null) {
+            list.add(new FieldMessage("email", "E-mail já cadastrado"));
         }
 
             for (FieldMessage f : list) {
